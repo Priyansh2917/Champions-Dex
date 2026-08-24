a2a7080e72607dbbbc26046dd64e850612f68a21
Priyansh <72566330+spidey-2917@users.noreply.github.com>
2026-08-24 17:10:40 +0530

    betting

--- ballsdex/core/bot.py
+++ ballsdex/core/bot.py
@@ -63,6 +63,7 @@ DEFAULT_PACKAGES = (
     ("money", "ballsdex.packages.money"),
     ("players", "ballsdex.packages.players"),
     ("trade", "ballsdex.packages.trade"),
+    ("betting", "ballsdex.packages.betting"),
 )
 
 
